68d5d94c018cd796917dece18410a1cf5879c088
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2018-10-21 17:42:57 -0500

    Adjusting Ruby environment for reality

--- ruby-env/run.sh
+++ ruby-env/run.sh
@@ -1,3 +1,6 @@
+echo "Building Container"
+docker build --tag ruby-dev .
+
 TARGET_DIR=$1
 echo "Running container with directory $TARGET_DIR"
-docker run --rm -it -v $TARGET_DIR:/app ruby
+docker run --rm -it -v $TARGET_DIR:/app ruby-dev

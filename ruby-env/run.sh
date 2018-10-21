e0394b8f1bf18db52818e3f7f9722cedace35520
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2018-10-21 17:49:01 -0500

    Extracting image tag into a variable

--- ruby-env/run.sh
+++ ruby-env/run.sh
@@ -1,6 +1,8 @@
+IMAGE_TAG=ruby-dev
+
 echo "Building Container"
-docker build --tag ruby-dev .
+docker build --tag $IMAGE_TAG .
 
 TARGET_DIR=$1
 echo "Running container with directory $TARGET_DIR"
-docker run --rm -it -v $TARGET_DIR:/app ruby-dev
+docker run --rm -it -v $TARGET_DIR:/app $IMAGE_TAG

f803865f70f3900770b83a2111590b0a43594bf5
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2018-10-21 19:08:23 -0500

    Adding golang CLI image and run files

---
+++ golang-env/run.sh
@@ -0,0 +1,8 @@
+IMAGE_TAG=ruby-dev
+
+echo "Building Container"
+docker build --tag $IMAGE_TAG .
+
+TARGET_DIR=$1
+echo "Running container with directory $TARGET_DIR"
+docker run --rm -it -v $TARGET_DIR:/app $IMAGE_TAG
